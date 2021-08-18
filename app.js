f23f715278fa5820bb9e01d01b2f1fca22173c6d
Cat Matteson <catmatteson@gmail.com>
2021-08-18 18:24:09 +1000

    updated styles

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let vid = document.querySelector(".video1");
 let vid2 = document.querySelector(".video2");
-vid.onended = function() {
-    vid.style.display = "none";
-    vid2.style.display = "initial";
+vid.onended = function () {
+  vid.style.display = "none";
+  vid2.style.display = "initial";
 };
